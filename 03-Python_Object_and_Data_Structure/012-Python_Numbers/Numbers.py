a15b6d823b8d0de7842ecff86d29469c9765bb39
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-24 12:32:26 -0700

    completed module 012

---
+++ 03-Python_Object_and_Data_Structure/012-Python_Numbers/Numbers.py
@@ -0,0 +1,23 @@
+print(2+1) # addition
+print(2-1) # subtraction
+print(2*2) # multiplication
+print(3/2) # division
+
+print("######################")
+
+print(7 / 4)
+print(7 % 4) # modulo
+
+print(50 % 5)
+
+print(23 % 2) # check if even or odd number
+print(20 % 2)
+
+print("######################")
+
+print(2 ** 3) # powers
+
+print("######################")
+
+print(2 + 10 * 10 + 3) # order of operations
+print((2 + 10) * (10 + 3))
