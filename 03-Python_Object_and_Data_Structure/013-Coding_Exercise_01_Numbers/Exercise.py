4af145011e5d451fee46ebda439723c3c94dfb00
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-24 13:01:14 -0700

    completed module 013, 014, 015

---
+++ 03-Python_Object_and_Data_Structure/013-Coding_Exercise_01_Numbers/Exercise.py
@@ -0,0 +1,12 @@
+# Write an expression that equals 100
+print(10 * 10)
+
+print(200 / 2)
+
+print(75 + 25)
+
+print(150 - 50)
+
+print((26 - 16) * (35 - 25))
+
+print(1 / 2)
---
+++ 03-Python_Object_and_Data_Structure/015-Quiz_01_Numbers/main.py
@@ -0,0 +1,8 @@
+# Which one of these is a floating point number?
+print(4.0 - 0.8)
+
+# Which of these will output the result 36
+print(6 * 6)
+
+# In python 3, what is the output of 1/2
+print(1 / 2)
