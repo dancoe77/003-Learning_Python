c4b3af14ec47766731792d31ce031c43bcc7bf52
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-25 23:39:52 -0700

    completed module 016

---
+++ 03-Python_Object_and_Data_Structure/016-Variable_assignments/main.py
@@ -0,0 +1,34 @@
+my_dogs = 2
+print(my_dogs)
+
+a = 5
+print(a) # 5
+
+a = 10
+print(a) # 10
+
+print(a + a) # 20
+
+a = a + a
+print(a) # 20
+
+a = a + a
+print(a) # 40
+
+a = a + a
+print(a) # 80
+
+print(type(a)) # class int
+
+a = 30.1
+print(a)
+print(type(a))
+
+# This won't work
+# int = 4
+
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes)
+print(type(my_taxes))
