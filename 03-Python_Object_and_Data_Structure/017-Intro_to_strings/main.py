6a2934d1aa546e014e16f4049da0dc8dc8323591
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-25 23:51:21 -0700

    completed module 017

---
+++ 03-Python_Object_and_Data_Structure/017-Intro_to_strings/main.py
@@ -0,0 +1,16 @@
+print('hello')
+
+print("world")
+
+print("this is also a string")
+
+print("I'm going on a run")
+
+print("hello world one")
+print("hello world two")
+
+print("hello \nworld")
+
+print("hello \tworld")
+
+print(len("Hello World"))
