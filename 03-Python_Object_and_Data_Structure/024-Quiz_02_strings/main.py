f09eb702840c713e6c603ebbcef07e6207fe29bc
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-26 21:46:34 -0700

    completed module 024

---
+++ 03-Python_Object_and_Data_Structure/024-Quiz_02_strings/main.py
@@ -0,0 +1,19 @@
+a = "Strings are immutable"
+print(a)
+b = "True"
+print(b)
+
+c = "##################################"
+print(c)
+
+d = "If s='hello' what is the output of s[1]"
+print(d)
+s = "hello"
+print(s[1]) # e
+
+print(c)
+
+e = "If s='Sammy' what is the output of s[2:]?"
+print(e)
+s = "Sammy"
+print(s[2:]) # mmy
