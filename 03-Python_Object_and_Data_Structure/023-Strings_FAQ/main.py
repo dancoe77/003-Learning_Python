db3477ea09dd68f38c1fdfda21fb386c46d3c5bf
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-26 21:39:57 -0700

    completed module 023

---
+++ 03-Python_Object_and_Data_Structure/023-Strings_FAQ/main.py
@@ -0,0 +1,9 @@
+a = "Are strings mutable?"
+print(a)
+b = "Strings are not mutable! (meaning you can't use indexing to change individual elements of a string)"
+print(b)
+
+c = "How do I create comments in my code?"
+print(c)
+d = "You can use the hashtag # to create comments in your code"
+print(d)
