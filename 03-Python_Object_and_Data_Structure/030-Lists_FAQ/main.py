9fa2cef6efc902cf46eb3690851c491ec1f657f5
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-27 17:28:41 -0700

    completed module 030

---
+++ 03-Python_Object_and_Data_Structure/030-Lists_FAQ/main.py
@@ -0,0 +1,6 @@
+question = "How do I index a nested list? For example if I want to grab 2 from [1,1,[1,2]]?"
+answer1 = "You would just add another set of brackets for indexing the nested list, for example: my_list[2][1]."
+answer2 = "We'll discover later on more nested objects and you will be quizzed on them later!)"
+print(question)
+print(answer1)
+print(answer2)
