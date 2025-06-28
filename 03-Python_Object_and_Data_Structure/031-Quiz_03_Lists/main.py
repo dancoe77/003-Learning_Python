67930ac462b3e54bd3209c2c8d661a6f7f4c6c0d
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-27 17:36:01 -0700

    completed module 031

---
+++ 03-Python_Object_and_Data_Structure/031-Quiz_03_Lists/main.py
@@ -0,0 +1,18 @@
+lst = [0,1,2]
+print(lst)
+popped_lst = lst.pop()
+print(popped_lst)
+print(lst)
+
+b = "#########################################"
+print(b)
+
+q = "Lists can have multiple object types."
+a = ("True")
+print(q)
+print(a)
+
+print(b)
+
+lst = ["a","b","c"]
+print(lst[1:])
