df8522152cc7cbacd3d40aea6bd348013f217bdc
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-28 20:16:50 -0700

    completed module 034

---
+++ 03-Python_Object_and_Data_Structure/034-Dictionaries_FAQ/main.py
@@ -0,0 +1,11 @@
+q1 = "Do dictionaries keep an order? "
+q2 = "How do I print the values of the dictionary in order?"
+
+a1 = "Dictionaries are mappings and do not retain order!"
+a2 = "If you do want the capabilities of a dictionary but you would like ordering as well, check out the ordereddict object lecture later on in the course!"
+
+print(q1)
+print(q2)
+
+print(a1)
+print(a2)
