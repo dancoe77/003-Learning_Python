001bcaa5b25c7baa243e7ce575bcfa9d38be104b
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-28 20:26:10 -0700

    completed module 035

---
+++ 03-Python_Object_and_Data_Structure/035-Quiz_04_Dictionaries/main.py
@@ -0,0 +1,24 @@
+q1 = "Is this statement True or False? "
+q2 = "Dictionaries retain order and are a sequence."
+
+a1 = "False"
+print(q1)
+print(q2)
+print(a1)
+
+b = "#######################################################"
+print(b)
+
+q1 = "Given d={'k1':[1,2,3]}"
+q2 = "What is the output of d['k1'][1]"
+print(q1)
+print(q2)
+d = {"k1":[1,2,3]}
+print(d["k1"][1])
+
+print(b)
+
+q1 = "Is this statement True or False? Dictionaries are immutable."
+a1 = "False"
+print(q1)
+print(a1)
