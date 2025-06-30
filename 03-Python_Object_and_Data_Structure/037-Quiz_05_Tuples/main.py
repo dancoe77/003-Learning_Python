63654e76e2954ff81227a8cffdd68c0d3895dd5f
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-30 14:23:13 -0700

    completed module 037

---
+++ 03-Python_Object_and_Data_Structure/037-Quiz_05_Tuples/main.py
@@ -0,0 +1,19 @@
+q1 = "Tuples have a lot of methods associated with them."
+a1 = "False"
+print(q1)
+print(a1)
+
+b = "########################################################"
+print(b)
+
+q2 = "Tuples are Immutable."
+a2 = "True"
+print(q2)
+print(a2)
+
+print(b)
+
+q3 = "Which of the following is a tuple?"
+a3 = "(1,2,[1,2])"
+print(q3)
+print(a3)
