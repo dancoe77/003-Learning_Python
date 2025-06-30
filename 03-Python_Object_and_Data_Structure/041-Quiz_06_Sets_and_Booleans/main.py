89e0bc320c494533df096ed42d534420c4b1b2c9
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-30 15:33:48 -0700

    completed module 041

---
+++ 03-Python_Object_and_Data_Structure/041-Quiz_06_Sets_and_Booleans/main.py
@@ -0,0 +1,15 @@
+q1 = "{1,2,3,4} is an example of a Set."
+a1 = "True" # {1,2,3,4} is the return when a set is called
+print(q1)
+print(a1)
+
+b = "##############################################"
+print(b)
+
+q2 = "How do you add an element to a set?"
+a2 = ".add()"
+
+print(b)
+
+q3 = "What is the result of set([1,1,2,3])"
+a3 = "{1,2,3}"
