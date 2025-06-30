79b14e70f2b9e868d5be6d72c87fbfceef7477f2
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-30 14:32:57 -0700

    completed module 038

---
+++ 03-Python_Object_and_Data_Structure/038-Sets_in_Python/main.py
@@ -0,0 +1,18 @@
+myset = set()
+print(myset)
+myset.add(1)
+print(myset)
+
+myset.add(2)
+print(myset)
+
+myset.add(2) # can only add unique values to sets
+print(myset)
+
+b = "######################"
+print(b)
+
+mylist = [1,1,1,1,1,2,2,2,3,3,3,3]
+print(mylist)
+mylist_set = set(mylist)
+print(mylist_set)
