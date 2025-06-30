aaff17f5cdaace2bbb2f49db68cd9432f54f596d
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-30 14:59:39 -0700

    completed module 040

---
+++ 03-Python_Object_and_Data_Structure/040-Booleans_in_Python/main.py
@@ -0,0 +1,19 @@
+t = True
+print(t)
+print(type(t))
+f = False
+print(f)
+print(type(f))
+
+b = "##########################"
+print(b)
+
+print(1 > 2)
+
+print(1 == 1)
+
+print(b)
+
+n = None
+print(n)
+print(type(n))
