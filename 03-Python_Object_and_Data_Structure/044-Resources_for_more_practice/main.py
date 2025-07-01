9daaccebf854266887a07c8c801c1e1fcd2c0dc1
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-30 17:11:34 -0700

    completed module 044

---
+++ 03-Python_Object_and_Data_Structure/044-Resources_for_more_practice/main.py
@@ -0,0 +1,35 @@
+r1 = "Basic Practice:"
+u1 = "http://codingbat.com/python"
+print(r1)
+print(u1)
+
+b = "#######################################"
+print(b)
+
+r2 = "More Mathematical (and Harder) Practice:"
+u2 = "https://projecteuler.net/archives"
+print(r2)
+print(u2)
+
+print(b)
+
+r3 = "List of Practice Problems:"
+u3 = "http://www.codeabbey.com/index/task_list"
+print(r3)
+print(u3)
+
+print(b)
+
+r4 = "A SubReddit Devoted to Daily Practice Problems:"
+u4 = "https://www.reddit.com/r/dailyprogrammer"
+print(r4)
+print(u4)
+
+print(b)
+
+r5 = "A very tricky website with very few hints and touch problems"
+u5 = "http://www.pythonchallenge.com/"
+print(r5)
+print(u5)
+
+print(b)
