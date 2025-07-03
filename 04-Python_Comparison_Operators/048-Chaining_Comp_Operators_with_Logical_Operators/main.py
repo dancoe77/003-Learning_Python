b2215bed7432eb3949e397971e6919859048a9a9
Dan Shoemaker <dan@shoeman-tech.com>
2025-07-02 21:40:09 -0700

    completed module 048

---
+++ 04-Python_Comparison_Operators/048-Chaining_Comp_Operators_with_Logical_Operators/main.py
@@ -0,0 +1,58 @@
+b = "##############################"
+print(b)
+
+print("1 < 2")
+print(1 < 2)
+print(b)
+
+print("2 < 3")
+print(2 < 3)
+print(b)
+
+print("1 < 2 < 3")
+print(1 < 2 < 3)
+print(b)
+
+print("1 < 2 > 3")
+print(1 < 2 > 3)
+print(b)
+
+print("1 < 2 and 2 > 3")
+print(1 < 2 and 2 > 3)
+print(b)
+
+print("1 < 2 and 2 < 3")
+print(1 < 2 and 2 < 3)
+print(b)
+
+print("'h' == 'h' and 2 == 2")
+print("h" == "h" and 2 == 2)
+print(b)
+
+print("1 == 1 or 2 == 2")
+print(1 == 1 or 2 == 2)
+print(b)
+
+print("100 == 1 or 2 == 2")
+print(100 == 1 or 2 == 2)
+print(b)
+
+print("100 == 1 or 2 == 200")
+print(100 == 1 or 2 == 200)
+print(b)
+
+print("1 == 1")
+print(1 == 1)
+print(b)
+
+print("not(1 == 1)")
+print(not(1 == 1))
+print(b)
+
+print("400 > 5000")
+print(400 > 5000)
+print(b)
+
+print("not(400 > 5000)")
+print(not(400 > 5000))
+print(b)
