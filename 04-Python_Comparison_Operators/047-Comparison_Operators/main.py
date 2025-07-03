05d07016f516b7d472973eea07212a8dab4dce5e
Dan Shoemaker <dan@shoeman-tech.com>
2025-07-02 21:22:54 -0700

    completed module 047

---
+++ 04-Python_Comparison_Operators/047-Comparison_Operators/main.py
@@ -0,0 +1,57 @@
+b = "########################"
+print(b)
+print("2 == 2")
+print(2 == 2)
+print(b)
+
+print("2 == 1")
+print(2 == 1)
+print(b)
+
+print("'hello' == 'bye'")
+print("hello" == "bye")
+print(b)
+
+print("'Bye' == 'bye'")
+print("Bye" == "bye")
+print(b)
+
+print("'2' == 2")
+print("2" == 2)
+print(b)
+
+print("2.0 == 2")
+print(2.0 == 2)
+print(b)
+
+print("3 != 3")
+print(3 != 3)
+print(b)
+
+print("4 != 5")
+print(4 != 5)
+print(b)
+
+print("2 > 1")
+print(2 > 1)
+print(b)
+
+print("1 > 2")
+print(1 > 2)
+print(b)
+
+print("1 < 2")
+print(1 < 2)
+print(b)
+
+print("2 < 5")
+print(2 < 5)
+print(b)
+
+print("2 >= 2")
+print(2 >= 2)
+print(b)
+
+print("4 <= 1")
+print(4 <= 1)
+print(b)
