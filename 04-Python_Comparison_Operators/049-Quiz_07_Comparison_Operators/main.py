9d1ffd5fbfed020a86eea4e4e7c48c90c7fc9b82
Dan Shoemaker <dan@shoeman-tech.com>
2025-07-02 21:51:48 -0700

    completed module 049

---
+++ 04-Python_Comparison_Operators/049-Quiz_07_Comparison_Operators/main.py
@@ -0,0 +1,39 @@
+b = "#############################################################"
+print(b)
+
+q1 = "What Boolean will be the output of the following: 2 < 4"
+a1 = "True"
+print(q1)
+print(a1)
+print(2 < 4)
+print(b)
+
+q2 = "What Boolean will be the output of the following: a= 12 b = a-10 a > b"
+a2 = "True"
+print(q2)
+print(a2)
+a = 12
+c = a - 10
+print(a > c)
+print(b)
+
+q3 = "What Boolean will be the output of the following: 12 != 12"
+a3 = "False"
+print(q3)
+print(a3)
+print(12 != 12)
+print(b)
+
+q4 = "What Boolean will be the output of the following: 2 < 3 > 10"
+a4 = "False"
+print(q4)
+print(a4)
+print(2 < 3 > 10)
+print(b)
+
+q5 = "What Boolean will be the output of the following: 2 <= 3 >= 1"
+a5 = "True"
+print(q5)
+print(a5)
+print(2 <= 3 >= 1)
+print(b)
