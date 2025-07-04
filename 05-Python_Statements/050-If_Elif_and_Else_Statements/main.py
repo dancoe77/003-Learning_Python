a27ac2bebdb027949b2568a944bf433a01501d9a
Dan Shoemaker <dan@shoeman-tech.com>
2025-07-03 18:17:44 -0700

    completed module 050

---
+++ 05-Python_Statements/050-If_Elif_and_Else_Statements/main.py
@@ -0,0 +1,93 @@
+b = "#######################################"
+print(b)
+if True:
+    print("IT'S TRUE!")
+print(b)
+
+if 3 > 2:
+    print("IT'S TRUE!")
+print(b)
+
+hungry = True
+if hungry:
+    print("FEED ME!")
+print(b)
+
+hungry = False
+if hungry:
+    print("FEED ME!")
+else:
+    print("I'm not hungry")
+print(b)
+
+hungry = True
+if hungry:
+    print("FEED ME!")
+else:
+    print("I'm not hungry")
+print(b)
+
+loc = "Bank"
+if loc == "Auto Shop":
+    print("Cars are cool!")
+else:
+    print("I do not know much.")
+print(b)
+
+loc = "Bank"
+if loc == "Auto Shop":
+    print("Cars are cool!")
+elif loc == "Bank":
+    print("Money is cool!")
+else:
+    print("I do not know much.")
+print(b)
+
+loc = "Store"
+if loc == "Auto Shop":
+    print("Cars are cool!")
+elif loc == "Bank":
+    print("Money is cool!")
+elif loc == "Store":
+    print("Welcome to the store!")
+else:
+    print("I do not know much.")
+print(b)
+
+loc = "Auto Shop"
+if loc == "Auto Shop":
+    print("Cars are cool!")
+elif loc == "Bank":
+    print("Money is cool!")
+elif loc == "Store":
+    print("Welcome to the store!")
+else:
+    print("I do not know much.")
+print(b)
+
+name = "Sammy"
+if name == "Frankie":
+    print("Hello Frankie!")
+elif name == "Sammy":
+    print("Hello Sammy!")
+else:
+    print("What is your name?")
+print(b)
+
+name = "Frankie"
+if name == "Frankie":
+    print("Hello Frankie!")
+elif name == "Sammy":
+    print("Hello Sammy!")
+else:
+    print("What is your name?")
+print(b)
+
+name = "Dan"
+if name == "Frankie":
+    print("Hello Frankie!")
+elif name == "Sammy":
+    print("Hello Sammy!")
+else:
+    print("What is your name?")
+print(b)
