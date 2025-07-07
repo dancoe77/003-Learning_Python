22d459e28536dd3a72acd5af21d3e2d3f12b20b9
Dan Shoemaker <dan@shoeman-tech.com>
2025-07-06 19:46:55 -0700

    completed module 057

---
+++ 06-Methods_and_Functions/057-Methods_and_Python_Documentation/main.py
@@ -0,0 +1,11 @@
+zz = "###############################"
+print(zz)
+mylist = [0,1,2,3,4]
+print(mylist)
+mylist.append(5)
+print(mylist)
+print(mylist.pop())
+print(mylist)
+print(zz)
+
+#help(mylist.insert())
