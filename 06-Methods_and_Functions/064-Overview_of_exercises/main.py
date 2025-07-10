0aff3b912eb21473fe069f353386b50b611060ac
Dan Shoemaker <dan@shoeman-tech.com>
2025-07-09 19:37:41 -0700

    completed module 064

---
+++ 06-Methods_and_Functions/064-Overview_of_exercises/main.py
@@ -0,0 +1,13 @@
+zz = "############################################################"
+print(zz)
+
+aa = "We have 3 main parts of exercises."
+bb = "Part 1: 10 In Course Coding Exercises"
+cc = "Part 2: Function Practice Exercises"
+dd = "Part 3:  Function and Methods Homework"
+
+print(aa)
+print(bb)
+print(cc)
+print(dd)
+print(zz)
