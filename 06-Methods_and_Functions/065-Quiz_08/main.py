0694eba085587fb784d67135ffb525be2985ae16
Dan Shoemaker <dan@shoeman-tech.com>
2025-07-09 19:42:02 -0700

    completed module 065

---
+++ 06-Methods_and_Functions/065-Quiz_08/main.py
@@ -0,0 +1,12 @@
+zz = "##################################################################"
+print(zz)
+
+q1a = "This is an automated check to make sure you saw the solutions link, thanks!"
+q1b = "https://docs.google.com/document/d/181AMuP-V5VnSorl_q7p6BYd8mwXWBnsZY_sSPA8trfc/edit?usp=sharing"
+
+a1a = "Ok thanks!"
+
+print(q1a)
+print(q1b)
+print(a1a)
+print(zz)
