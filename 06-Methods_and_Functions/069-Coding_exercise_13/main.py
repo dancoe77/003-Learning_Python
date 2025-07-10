ff0d98a715ac55cbea2548c0a474182531476990
Dan Shoemaker <dan@shoeman-tech.com>
2025-07-10 12:17:13 -0700

    completed module 069

---
+++ 06-Methods_and_Functions/069-Coding_exercise_13/main.py
@@ -0,0 +1,14 @@
+zz = "#####################################"
+print(zz)
+
+def using_bools(x,y,z):
+    if z == True:
+        return x
+    else:
+        pass
+    return y
+
+print(using_bools("Milky Way Galaxy","Andromeda Galaxy",True))
+print(zz)
+print(using_bools("Milky Way Galaxy","Andromeda Galaxy",False))
+print(zz)
