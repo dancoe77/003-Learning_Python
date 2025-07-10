f8da8fb4e0447f3b0abc05ce929336ba5bd8e099
Dan Shoemaker <dan@shoeman-tech.com>
2025-07-10 12:27:51 -0700

    completed module 071

---
+++ 06-Methods_and_Functions/071-Coding_exercise_15/main.py
@@ -0,0 +1,13 @@
+zz = "####################################################"
+print(zz)
+
+def check_even(a):
+    if a%2 == 0:
+        return True
+    else:
+        pass
+    return False
+print(check_even(2))
+print(zz)
+print(check_even(5))
+print(zz)
