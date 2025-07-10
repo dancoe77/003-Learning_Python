a8e55498e3f647286aa2677d37316a5e19a0a7d3
Dan Shoemaker <dan@shoeman-tech.com>
2025-07-10 12:33:25 -0700

    completed module 072

---
+++ 06-Methods_and_Functions/072-Coding_exercise_16/main.py
@@ -0,0 +1,17 @@
+zz = "######################################"
+print(zz)
+
+def check_greater(a,b):
+    if a > b:
+        return True
+    else:
+        pass
+    return False
+print(check_greater(2,5))
+print(zz)
+print(check_greater(7,3))
+print(zz)
+print(check_greater(201,200))
+print(zz)
+print(check_greater(125,521))
+print(zz)
