9a484047cc8f07b458cd67d42cb75dcc2f0a952d
Dan Shoemaker <dan@shoeman-tech.com>
2025-07-10 11:59:20 -0700

    completed module 068

---
+++ 06-Methods_and_Functions/067-Coding_exercise_11/main.py
@@ -0,0 +1,8 @@
+zz = "#######################################################"
+print(zz)
+
+def print_name(name):
+    print(f"Hello {name}")
+
+print_name("Dan")
+print(zz)
---
+++ 06-Methods_and_Functions/068-Coding_exercise_12/main.py
@@ -0,0 +1,14 @@
+zz = "######################################"
+print(zz)
+
+def simple_bool(a):
+    if a == True:
+        return "Hello"
+    else:
+        pass
+    return "Goodbye"
+
+print(simple_bool(True))
+print(zz)
+print(simple_bool(False))
+print(zz)
