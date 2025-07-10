ff82455e3f8e93ce163a369488055b3f87d8dcd9
Dan Shoemaker <dan@shoeman-tech.com>
2025-07-10 12:22:39 -0700

    completed module 070

---
+++ 06-Methods_and_Functions/070-Coding_exercise_14/main.py
@@ -0,0 +1,11 @@
+zz = "################################################"
+print(zz)
+
+def sum_ints(a,b):
+    return a + b
+print(sum_ints(2,5))
+print(zz)
+print(sum_ints(25,35))
+print(zz)
+print(sum_ints(69,11))
+print(zz)
