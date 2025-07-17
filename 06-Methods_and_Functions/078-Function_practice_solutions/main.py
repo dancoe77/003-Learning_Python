878763cab725ac6adea30fa138cf1147555bc96f
Dan Shoemaker <dan@shoeman-tech.com>
2025-07-16 17:30:45 -0700

    completed Level 1 problems

--- 06-Methods_and_Functions/078-Function_practice_solutions/main.py
+++ 06-Methods_and_Functions/078-Function_practice_solutions/main.py
@@ -1,6 +1,8 @@
 zz = "##########################################################"
 print(zz)
 
+print("WARMUP")
+print(zz)
 def lesser_of_two_evens(a,b):
     if a%2 == 0 and b%2 == 0:
         if a < b:
@@ -37,4 +39,37 @@ def makes_twenty(int1,int2):
 print(makes_twenty(20,10))
 print(makes_twenty(12,8))
 print(makes_twenty(2,3))
+print(zz)
+
+print("Lvl 1 Problems")
+print(zz)
+
+def old_macdonald(name):
+    if len(name) > 3:
+        return name[:3].capitalize() + name[3:].capitalize()
+    else:
+        return "Name is too short!"
+print(old_macdonald("macdonald"))
+print(zz)
+
+def master_yoda(text):
+    s = (text.split()[::-1])
+    delimiter = " "
+    result = delimiter.join(s)
+    return result
+print(master_yoda("I am home"))
+print(master_yoda("We are ready"))
+print(zz)
+
+def almost_there(int):
+    if abs(100 - int) <=10:
+        return True
+    elif abs(200 - int) <=10:
+        return True
+    else:
+        return False
+print(almost_there(90))
+print(almost_there(104))
+print(almost_there(150))
+print(almost_there(200))
 print(zz)
