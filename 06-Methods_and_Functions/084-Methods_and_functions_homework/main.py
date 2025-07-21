3f51a91d8725e866b2e92a5f044ed11620e44d30
Dan Shoemaker <dan@shoeman-tech.com>
2025-07-21 12:32:43 -0700

    completed module 084

---
+++ 06-Methods_and_Functions/084-Methods_and_functions_homework/main.py
@@ -0,0 +1,58 @@
+zz = "###########################################"
+print(zz)
+
+aa = "Write a function that computes the volume of a sphere given its radius."
+aa1 = "The volume of a sphere is given as"
+aa2 = "4/3*pi*r^3"
+print(aa)
+print(aa1)
+print(aa2)
+print(zz)
+
+bb = "Write a function that checks whether a number is in a given range (inclusive of high and low)"
+print(bb)
+print(zz)
+
+cc = "Write a Python function that accepts a string and calculates the number of upper case letters and lower case letters."
+cc1 = "Sample String : 'Hello Mr. Rogers, how are you this fine Tuesday?'"
+cc2 = "No. of Upper case characters : 4"
+cc3 = "No. of Lower case Characters : 33"
+print(cc)
+print(cc1)
+print(cc2)
+print(cc3)
+print(zz)
+
+dd = "Write a Python function that takes a list and returns a new list with unique elements of the first list."
+dd1 = "Sample List : [1,1,1,1,2,2,3,3,3,3,4,5]"
+dd2 = "Unique List : [1, 2, 3, 4, 5]"
+print(dd)
+print(dd1)
+print(dd2)
+print(zz)
+
+ee = "Write a Python function that checks whether a word or phrase is palindrome or not."
+ee1 = "Note: A palindrome is word, phrase, or sequence that reads the same backward as forward,"
+ee2 = "e.g., madam,kayak,racecar, or a phrase 'nurses run'"
+ee3 = "Hint: You may want to check out the .replace() method in a string to help out with dealing with spaces."
+ee4 = "Also google search how to reverse a string in Python, there are some clever ways to do it with slicing notation."
+print(ee)
+print(ee1)
+print(ee2)
+print(ee3)
+print(ee4)
+print(zz)
+
+ff = "Write a Python function to check whether a string is pangram or not. (Assume the string passed in does not have any punctuation)"
+ff1 = "Note : Pangrams are words or sentences containing every letter of the alphabet at least once."
+ff2 = "For example : 'The quick brown fox jumps over the lazy dog'"
+ff3 = "Hint: You may want to use .replace() method to get rid of spaces."
+ff4 = "Hint: Look at the string module"
+ff5 = "Hint: In case you want to use set comparisons"
+print(ff)
+print(ff1)
+print(ff2)
+print(ff3)
+print(ff4)
+print(ff5)
+print(zz)
