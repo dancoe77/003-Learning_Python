326d363a57d83ec9d191518ad481191b25b82c8d
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-01 19:44:00 -0700

    completed module 086

---
+++ 07-Milestone_Project_1/086-Intro_to_warmup_project_exercises/main.py
@@ -0,0 +1,18 @@
+zz = "#######################################################################"
+print(zz)
+
+aa = "Visual Representation"
+bb = "User Input"
+cc = "Function"
+dd = "Updates"
+ee = "New Visual"
+ff = "Updated Visual Represntation"
+print(aa)
+print(bb)
+print(cc)
+print(dd)
+print(ee)
+print(ff)
+
+print(zz)
+
