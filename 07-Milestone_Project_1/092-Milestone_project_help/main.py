24e1d9c6c6d2d1e2fed0e13dafb44562a9b31f38
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-04 00:46:09 -0700

    completed module 092

---
+++ 07-Milestone_Project_1/092-Milestone_project_help/main.py
@@ -0,0 +1,20 @@
+zz = "############################################"
+print(zz)
+
+aa = "First off, make sure you understand the project scope"
+bb = "What needs to happen?"
+print(aa)
+print(bb)
+print(zz)
+
+cc = "We need to print a board"
+dd = "Take in player input"
+ee = "Place their input on the board"
+ff = "Check if the game is won, tied, lost, or ongoing"
+gg = "Repeat c and d until the game has been won or tied"
+print(cc)
+print(dd)
+print(ee)
+print(ff)
+print(gg)
+print(zz)
