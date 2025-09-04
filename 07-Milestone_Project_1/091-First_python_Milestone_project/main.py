ed1724ea5a408341c2e6b180e2b0483213311c6f
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-04 00:40:49 -0700

    completed module 091

---
+++ 07-Milestone_Project_1/091-First_python_Milestone_project/main.py
@@ -0,0 +1,25 @@
+zz = "##########################################"
+print(zz)
+
+aa = "Two players should be able to play the game (both sitting at the same computer)"
+bb = "The board should be printed out every time a player makes a move"
+cc = "We should be able to accept input of the player position and then place a symbol on the board"
+print(aa)
+print(bb)
+print(cc)
+print(zz)
+
+dd = "We will use the number pad to match numbers to the grid on a tic tac toe board"
+ee = [7,8,9]
+ff = [4,5,6]
+gg = [1,2,3]
+print(ee)
+print(ff)
+print(gg)
+print(zz)
+
+hh = "Let's explore what the game will look like once it's done"
+ii = "We'll also cover a few useful functions and go through the walktrough notebook"
+print(hh)
+print(ii)
+print(zz)
