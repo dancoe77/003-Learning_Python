3fd706655944702fd04176ae2947db696642c48d
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-04 19:32:51 -0700

    completed module 093

---
+++ 07-Milestone_Project_1/093-Solution_for_Milestone_project_part_1/main.py
@@ -0,0 +1,82 @@
+from IPython.display import clear_output
+
+def display_board(board):
+    clear_output()
+    print(board[7]+"|"+board[8]+"|"+board[9])
+    print(board[4]+"|"+board[5]+"|"+board[6])
+    print(board[1]+"|"+board[2]+"|"+board[3])
+#test_board = ["#","X","O","X","O","X","O","X","O","X"]
+#display_board(test_board)
+
+def player_input():
+
+    '''
+    OUTPUT = (Player 1 marker, Player 2 marker)
+    '''
+
+    marker = ""
+
+    while marker != "X" and marker != "O":
+        marker = input("Player1: Choose X or O: ").upper()
+
+    if marker == "X":
+        return ("X","O")
+    else:
+        return ("O","X")
+#player1_marker , player2_marker = player_input()
+#print(player1_marker)
+#print(player2_marker)
+
+def place_marker(board, marker, position):
+    board[position] = marker
+#test_board = ["#","X","O","X","O","X","O","X","O","X"]
+#place_marker(test_board,"$",8)
+#display_board(test_board)
+
+def win_check(board, mark):
+
+    # WIN TAC TOE?
+
+    # ALL ROWS, and check to see if they share all the same marker?
+    return ((board[7] == mark and  board[8] == mark and  board[9] == mark ) or
+            (board[4] == mark and  board[5] == mark and  board[6] == mark ) or
+            (board[1] == mark and  board[2] == mark and  board[3] == mark ) or
+            # ALL COLUMNS, check to see if marker matches
+            (board[7] == mark and  board[4] == mark and  board[1] == mark) or
+            (board[8] == mark and  board[5] == mark and  board[2] == mark) or
+            (board[9] == mark and  board[6] == mark and  board[3] == mark) or
+            # Two diagonals
+            (board[7] == mark and  board[5] == mark and  board[3] == mark) or
+            (board[9] == mark and  board[5] == mark and  board[1] == mark))
+#test_board = ["#","X","O","X","O","X","O","X","O","X"]
+#display_board(test_board)
+#print(win_check(test_board,"X"))
+
+import random
+
+def choose_first():
+    flip = random.randint(0,1)
+    if flip == 0:
+        return "Player 1"
+    else:
+        return "Player 2"
+
+def space_check(board, position):
+    return board[position] == " "
+
+def full_board_check(board):
+    for i in range(1,10):
+        if space_check(board, i):
+            return False
+    # Board is full if we return True
+    return True
+
+def player_choice(board):
+    position = 0
+    while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
+        position = int(input("Choose a position: (1-9) "))
+    return position
+
+def replay():
+    choice = input("Play again? Enter Yes or No")
+    return choice == "Yes"
