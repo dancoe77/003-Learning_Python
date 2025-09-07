669394833f5388143d876bd3f1c459e0566e18ba
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-06 19:29:59 -0700

    completed module 095

---
+++ 08-Object_Oriented_Programming/095-OOP_Intro/main.py
@@ -0,0 +1,27 @@
+zz = "#######################################"
+print(zz)
+
+aa = "OOP allows programmers to create their own objects and attributes"
+bb = "After defining a string, list, dictionary, or other object we are ables to call methods off of them"
+print(aa)
+print(bb)
+print(zz)
+
+cc = "These methods act as functions that use information about the object, as well as the object itself, to return results or change the current object"
+dd = "For example this includes appending to a list, or counting the occurences of an element in a tuple"
+print(cc)
+print(dd)
+print(zz)
+
+ee = "OOP allows developers to create their own objects"
+ff = "In general, OOP allows us to create code that is repeatable and organized"
+print(ee)
+print(ff)
+print(zz)
+
+gg = "For much larger code functions by themselves aren't enough for organization and repeatability"
+hh = "Commonly repated tasks and objects can defined with OOP to create code that is more usable"
+print(gg)
+print(hh)
+print(zz)
+
