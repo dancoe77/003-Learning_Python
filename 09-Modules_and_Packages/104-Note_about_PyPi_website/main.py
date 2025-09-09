6f2753802d5ce7de3a847ea84c25fb17126267c8
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-09 14:28:26 -0700

    completed module 104

---
+++ 09-Modules_and_Packages/104-Note_about_PyPi_website/main.py
@@ -0,0 +1,8 @@
+zz = "#############################################"
+print(zz)
+
+aa = "https://pypi.org has changed its layout"
+bb = "The documentation can now be found linked in the upper right hand side under Docs"
+print(aa)
+print(bb)
+print(zz)
