365751be3b4fd3102b222ac6a3fe3fb5d32d06d0
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-10 23:58:44 -0700

    completed module 106

---
+++ 09-Modules_and_Packages/106-Modules_and_Packages/main.py
@@ -0,0 +1,14 @@
+zz = "########################################"
+print(zz)
+
+from mymodule import my_func
+my_func()
+print(zz)
+
+from MyMainPackage import some_main_script
+from MyMainPackage.SubPackage import mysubscript
+
+some_main_script.report_main()
+mysubscript.sub_report()
+print(zz)
+
