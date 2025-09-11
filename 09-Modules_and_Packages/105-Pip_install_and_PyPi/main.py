9d77f6f365d6ad299744124774f4b552d09cb997
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-10 17:40:45 -0700

    completed module 105

---
+++ 09-Modules_and_Packages/105-Pip_install_and_PyPi/main.py
@@ -0,0 +1,37 @@
+zz = "############################################################"
+print(zz)
+
+aa = "PyPi is a repository for open-source thrid-party Python packages"
+bb = "Similar to RubyGems in the Ruby world, PHP's Packagist, CPAN for Perl and NPM for Node.js"
+print(aa)
+print(bb)
+
+cc = "pip install requests"
+print(cc)
+dd = "Requirement already satisfied: requests in /usr/lib/python3.13/site-packages (2.32.4)"
+print(dd)
+print(zz)
+
+ee = "pip install coloroma"
+ff = "Successfully installed colorama-0.4.6"
+print(ee)
+print(ff)
+print(zz)
+
+gg = "from colorama import init"
+hh = "init()"
+ii = "from colorama import Fore"
+jj = "print(Fore.RED + 'some red text')"
+kk = "print(Fore.GREEN + 'Switch to green')"
+print(gg)
+print(hh)
+print(ii)
+print(jj)
+print(kk)
+print(zz)
+
+ll = "pip install openpyxl"
+mm = "Successfully installed et-xmlfile-2.0.0 openpyxl-3.1.5"
+print(ll)
+print(mm)
+print(zz)
