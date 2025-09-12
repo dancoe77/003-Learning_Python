ddd5f59590e3e4feede99c6134e987b239303be0
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-11 20:44:30 -0700

    completed module 109

---
+++ 10-Errors_and_Exception_Handling/109-Errors_and_Exceptions_Homework/main.py
@@ -0,0 +1,39 @@
+zz = "##########################################################"
+print(zz)
+
+aa = "Problem 1"
+bb = "Handle the exception thrown by the code below by using try and except blocks"
+print(aa)
+print(bb)
+print(zz)
+
+for i in ["a","b","c"]:
+    print(i**2)
+print(zz)
+
+cc = "Problem 2"
+dd = "Handle the exception thrown by the code by using try and except blocks"
+ee = "Then use a finally block to print 'All Done'"
+print(cc)
+print(dd)
+print(ee)
+print(zz)
+
+x = 5
+y = 0
+
+z = x/y
+print(zz)
+
+ff = "Problem 3"
+gg = "Write a function that asks for an integer and prints the square of it"
+hh = "Use a while loop with a try, except, else block to account for incorrect inputs"
+print(ff)
+print(gg)
+print(hh)
+print(zz)
+
+def ask():
+    pass
+ask()
+print(zz)
