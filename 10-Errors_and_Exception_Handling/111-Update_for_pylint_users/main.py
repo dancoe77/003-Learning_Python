44fae735f6682872c1b707feee0032936f21640b
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-11 21:38:15 -0700

    completed module 111

---
+++ 10-Errors_and_Exception_Handling/111-Update_for_pylint_users/main.py
@@ -0,0 +1,12 @@
+zz = "##########################################"
+print(zz)
+
+aa = "Quick update in the Pylint library"
+bb = "To see the same report that I do in the video, in the newest version of PyLint you need to add -r y (report yes) at the end of the command"
+cc = "pylint myexample.py -r y"
+dd = "Pylint mostly disabled negative scores by now."
+print(aa)
+print(bb)
+print(cc)
+print(dd)
+print(zz)
