fe1b2966b903a883379d2f2490e3d48120b23829
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-14 19:12:20 -0700

    completed module 112

---
+++ 10-Errors_and_Exception_Handling/112-Pylint_overview/main.py
@@ -0,0 +1,30 @@
+'''
+Simple program to test pylint
+'''
+
+ZZ = "###########################################"
+print(ZZ)
+
+AA = "There are several testing tools, we will focus on two:"
+BB = "pylint: This library will look at the code and report back possible issues"
+CC = "unittest: This built-in library will allow us to test our own programs and "
+DD = "check we are getting desired outputs"
+print(AA)
+print(BB)
+print(CC)
+print(DD)
+
+def myfunc():
+    '''
+    A simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+myfunc()
+print(ZZ)
+#A = 1
+#B = 2
+#print(A)
+#print(B)
