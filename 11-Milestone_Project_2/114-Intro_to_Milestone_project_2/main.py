75fe91e0a138731688157b29b5c3bbb2c351768d
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-16 21:34:49 -0700

    completed module 114

---
+++ 11-Milestone_Project_2/114-Intro_to_Milestone_project_2/main.py
@@ -0,0 +1,20 @@
+zz = "##################################"
+print(zz)
+
+aa = "To warm up we will recreate the card game War"
+print(aa)
+print(zz)
+
+bb = "War process can be repeated in the case of back to back ties"
+cc = "To construct this game, we will need:"
+dd = "- Card Class"
+ee = "- Deck class"
+ff = "- Player Class"
+gg = "- Game Logic"
+print(bb)
+print(cc)
+print(dd)
+print(ee)
+print(ff)
+print(gg)
+print(zz)
