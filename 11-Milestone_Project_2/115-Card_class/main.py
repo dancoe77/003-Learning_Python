ffa5e5611da6e6596129537cde2376e1be76cf9f
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-16 22:03:12 -0700

    completed module 115

---
+++ 11-Milestone_Project_2/115-Card_class/main.py
@@ -0,0 +1,30 @@
+zz = "###############################################################"
+print(zz)
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
+          'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+
+class Card:
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+    def __str__(self):
+        return self.rank + " of " + self.suit
+two_hearts = Card("Hearts","Two")
+print(type(two_hearts))
+print(two_hearts)
+print(two_hearts.suit)
+print(two_hearts.rank)
+print(two_hearts.value)
+print(zz)
+
+three_clubs = Card("Clubs","Three")
+print(three_clubs.suit)
+print(three_clubs.rank)
+print(three_clubs.value)
+print(zz)
+
+print(two_hearts.value < three_clubs.value)
+print(zz)
