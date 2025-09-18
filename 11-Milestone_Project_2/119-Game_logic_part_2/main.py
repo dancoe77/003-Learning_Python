d62811a4ec7430bf2b26ad52bdf17fb63192c324
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-17 22:04:10 -0700

    completed module 119

---
+++ 11-Milestone_Project_2/119-Game_logic_part_2/main.py
@@ -0,0 +1,75 @@
+import random
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
+          'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+class Card:
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+    def __str__(self):
+        return self.rank + " of " + self.suit
+
+class Deck:
+    def __init__(self):
+        self.all_cards = []
+        for suit in suits:
+            for rank in ranks:
+                # Create the Card object
+                created_card = Card(suit,rank)
+                self.all_cards.append(created_card)
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+    def deal_one(self):
+        return self.all_cards.pop()
+class Player:
+    def __init__(self,name):
+        self.name = name
+        self.all_cards = []
+    def remove_one(self):
+        return self.all_cards.pop(0)
+    def add_cards(self,new_cards):
+        if type(new_cards) == type([]):
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
+    def __str__(self):
+        return f"Player {self.name} has {len(self.all_cards)} cards."
+# Game setup
+player_one = Player("Dan")
+player_two = Player("Colista")
+
+new_deck = Deck()
+new_deck.shuffle()
+
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+# print(len(player_one.all_cards))
+# print(player_one.all_cards[0])
+
+game_on = True
+
+round_num = 0
+while game_on:
+    round_num += 1
+    print(f"Round {round_num}")
+
+    if len(player_one.all_cards) == 0:
+        print(f"{player_one}, out of cards! {player_two} wins!!")
+        game_on = False
+        break
+    if len(player_two.all_cards) == 0:
+        print(f"{player_two}, out of cards! {player_one} wins!!")
+        game_on = False
+        break
+
+    # Start a new round
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
+
+    # while at war
