1274a1c0e8e76a4d6b4c28e3291e76dd878a0a83
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-17 22:54:09 -0700

    completed module 121

---
+++ 11-Milestone_Project_2/121-Milestone_project_overview/main.py
@@ -0,0 +1,18 @@
+zz = "######################################################################"
+print(zz)
+
+aa = "BlackJack game with python"
+bb = "Computer Dealer and Human Player"
+cc = "Player places bet"
+dd = "The player can stand or hit."
+ee = "The player must be able to pick their betting amount."
+ff = "You need to keep track of the player's total money."
+gg = "You need to alert the player of wins, losses, or busts, etc..."
+print(aa)
+print(bb)
+print(cc)
+print(dd)
+print(ee)
+print(ff)
+print(gg)
+print(zz)
