546e173fba8e21192799408ad0253b7537c68b92
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-17 21:41:58 -0700

    completed module 118

---
+++ 11-Milestone_Project_2/118-Game_logic_part_1/main.py
@@ -0,0 +1,26 @@
+zz = "########################################################"
+print(zz)
+
+aa = "Player One and Player Two"
+bb = "New Deck"
+cc = "Shuffle"
+dd = "Split cards between two players"
+ee = "Check for zero cards"
+ff = "game_on = True"
+gg = "while game_on"
+hh = "pop card and compare - winner gets cards at -1"
+ii = "while at_war draw draw four cards and compare"
+jj = "winner gets all cards at -1 and check for game_on = False"
+kk = "Player two wins"
+print(aa)
+print(bb)
+print(cc)
+print(dd)
+print(ee)
+print(ff)
+print(gg)
+print(hh)
+print(ii)
+print(jj)
+print(kk)
+print(zz)
