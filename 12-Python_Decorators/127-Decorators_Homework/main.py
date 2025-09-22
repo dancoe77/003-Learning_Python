2d7a8388524994090c93f0fc8815f4ee538540d9
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-21 21:33:15 -0700

    completed module 127

---
+++ 12-Python_Decorators/127-Decorators_Homework/main.py
@@ -0,0 +1,5 @@
+zz = "###########################################################"
+print(zz)
+
+print("[django View decorators](https://docs.djangoproject.com/en/5.2/topics/http/decorators/)")
+print(zz)
