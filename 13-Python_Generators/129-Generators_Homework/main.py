3ee70f467d9d4a21c09cb72e623e0207a31bdf9a
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-24 22:44:42 -0700

    completed module 129

---
+++ 13-Python_Generators/129-Generators_Homework/main.py
@@ -0,0 +1,30 @@
+zz = "###############################################"
+print(zz)
+
+def gensquares(N):
+    pass
+for x in gensquares(10):
+    print(x)
+print(zz)
+
+import random
+random.randint(1,10)
+
+def ran_num(low,high,n):
+    pass
+for num in ran_num(1,10,12):
+    print(num)
+print(zz)
+
+s = "hello"
+
+print(zz)
+aa = "Explain a use case for a generator using a yield statement where we would not want to use a normal function with a return statement"
+
+print(zz)
+
+my_list = [1,2,3,4,5]
+gencomp = (item for item in my_list if item > 3)
+for item in gencomp:
+    print(item)
+print(zz)
