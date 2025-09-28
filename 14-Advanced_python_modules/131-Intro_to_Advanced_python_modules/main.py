24901003e8d1d908025436c3359e2a458fec2c72
Dan Shoemaker <dan@shoeman-tech.com>
2025-09-28 15:15:58 -0700

    completed module 131

---
+++ 14-Advanced_python_modules/131-Intro_to_Advanced_python_modules/main.py
@@ -0,0 +1,30 @@
+zz = "##########################################"
+print(zz)
+
+aa = "Python has several built-in modules"
+bb = "In this section we will be be diving deeper into the useful built-in modules"
+cc = "Explore some use cases"
+dd = "Followed by a puzzle exercise to solve"
+print(aa)
+print(bb)
+print(cc)
+print(dd)
+print(zz)
+
+ee = "Modules covered will be:"
+ff = "Collections"
+gg = "OS Module and Datetime"
+hh = "Math and Random"
+ii = "Python debugger"
+jj = "Timeit"
+kk = "Reg expressions"
+ll = "Unzipping and Zipping"
+print(ee)
+print(ff)
+print(gg)
+print(hh)
+print(ii)
+print(jj)
+print(kk)
+print(ll)
+print(zz)
