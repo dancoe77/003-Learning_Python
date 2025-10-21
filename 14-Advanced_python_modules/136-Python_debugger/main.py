bb17c20964fb9195bbbb215537898e8cf224d7ac
Dan Shoemaker <dan@shoeman-tech.com>
2025-10-21 00:26:18 -0700

    completed module 136

---
+++ 14-Advanced_python_modules/136-Python_debugger/main.py
@@ -0,0 +1,18 @@
+zz = "################################"
+print(zz)
+
+x = [1,2,3]
+y = 2
+z = 3
+result = y + z
+#result2 = x + y
+print(result)
+#print(result2)
+print(zz)
+
+import pdb
+result_one = y + z
+print(result_one)
+pdb.set_trace()
+result_two = y + x
+print(zz)
