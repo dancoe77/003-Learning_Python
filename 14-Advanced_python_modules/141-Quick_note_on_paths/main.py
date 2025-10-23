ef39635cdb82a15710f694987960ccf45cf23e1b
Dan Shoemaker <dan@shoeman-tech.com>
2025-10-22 19:56:56 -0700

    completed module 141

---
+++ 14-Advanced_python_modules/141-Quick_note_on_paths/main.py
@@ -0,0 +1,12 @@
+zz = "###################################################"
+print(zz)
+
+aa = "Quick Note On Paths"
+bb = "Please note you might need to adjust your path depending on your environment"
+cc = "Instead of C:\\Path\\to\\file; Windows OS"
+dd = "use /path/to/file; MacOS or Linux"
+print(aa)
+print(bb)
+print(cc)
+print(dd)
+print(zz)
