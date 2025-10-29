b341956efb2b7b6420dae2af9a281ac5441ea018
Dan Shoemaker <dan@shoeman-tech.com>
2025-10-28 22:25:02 -0700

    completed module 146

---
+++ 15-Web_Scraping/146-Setting_up_Web_scraping/main.py
@@ -0,0 +1,12 @@
+zz = "#############################################################"
+print(zz)
+
+import requests
+import bs4
+from bs4 import BeautifulSoup
+
+aa = "On page right-click 'View page source'"
+print(aa)
+bb = "On picture right-click 'Inspect'"
+print(bb)
+print(zz)
