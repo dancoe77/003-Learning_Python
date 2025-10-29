c8179f6204468c97f0e92e2916680edc462b5662
Dan Shoemaker <dan@shoeman-tech.com>
2025-10-28 22:10:09 -0700

    completed module 145

---
+++ 15-Web_Scraping/145-Intro_to_Web_Scraping/main.py
@@ -0,0 +1,10 @@
+zz = "##############################################"
+print(zz)
+
+aa = "HTML = Hypertext Markup Language"
+print(aa)
+
+bb = "CSS = Cascading Style Sheets"
+print(bb)
+
+print(zz)
