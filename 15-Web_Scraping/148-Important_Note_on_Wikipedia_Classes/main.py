21ebd01bd6490dc98353cb68215e8a52c80641ef
Dan Shoemaker <dan@shoeman-tech.com>
2025-10-28 23:04:00 -0700

    completed module 148

---
+++ 15-Web_Scraping/148-Important_Note_on_Wikipedia_Classes/main.py
@@ -0,0 +1,8 @@
+zz = "###################################################"
+print(zz)
+
+aa = "Due to recent changes in the HTML and CSS layout of Wikipedia pages, the classes shown in the video may no longer work as expected."
+print(aa)
+bb = "The .toctext class has been renamed to .vector-toc-text. Therefore, instead of using: soup.select('.toctext') try using soup.select('.vector-toc-text')"
+print(bb)
+print(zz)
