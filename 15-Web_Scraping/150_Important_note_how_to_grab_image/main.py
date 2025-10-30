ddbe35e17f29d82379dae915625e71fbb93e292d
Dan Shoemaker <dan@shoeman-tech.com>
2025-10-29 21:41:10 -0700

    completed module 150

---
+++ 15-Web_Scraping/150_Important_note_how_to_grab_image/main.py
@@ -0,0 +1,16 @@
+zz = "######################################################"
+print(zz)
+
+aa = "Important note on how to grab an image"
+print(aa)
+
+bb = "Similar to what was shown in grabbing a title, the image class names on Wikipedia have been updated"
+print(bb)
+
+cc = "Instead of using .thumbimage try using .mw-file-element"
+print(cc)
+dd = "Please note that this class may appear multiple times on a page so be sure to use the correct index"
+print(dd)
+ee = "As of now the correct index for this image is 'computer = image_info[1]'"
+print(ee)
+print(zz)
