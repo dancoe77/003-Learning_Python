9ff0a6ad5514bbf4f4d54e661e4090a5d3c4f4ed
Dan Shoemaker <dan@shoeman-tech.com>
2025-11-05 21:23:19 -0800

    completed module 154

---
+++ 15-Web_Scraping/154-PWS_Exercise_Overview/main.py
@@ -0,0 +1,16 @@
+zz = "############################################################"
+print(zz)
+
+bb = "TASK: Import any libraries you think you'll need to scrape a website"
+print(bb)
+cc = "TASK: Use requests library and BeautifulSoup library to connect to https://quotes.toscrape.com and get the HTML text from the page"
+print(cc)
+dd = "TASK: Get the names of all the authors on the first page"
+print(dd)
+ee = "TASK: Create a list of all the quotes on the first page"
+print(ee)
+ff = "TASK: Inspect the site and use bs4 to extract the top ten tags from the requests text shown on the top right of the page (e.g Love, Inspirational, Life, etc...)"
+print(ff)
+gg = "TASK: Use what you know about for loops and string concatenation to loop through all the pages and get all the unique authors on the website"
+print(gg)
+print(zz)
