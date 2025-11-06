c4e7b5971c781f7e2206f2205e32565aa87a2911
Dan Shoemaker <dan@shoeman-tech.com>
2025-11-06 15:33:05 -0800

    completed module 156

---
+++ 16-Working_with_Images/156-Intro_to_Images/main.py
@@ -0,0 +1,8 @@
+zz = "#########################################################"
+print(zz)
+
+aa = "We will focus on learning how to work with images using the Pillow library."
+print(aa)
+bb = "We will install it using pip install pillow and then show you how to open, save and interact with image files"
+print(bb)
+print(zz)
