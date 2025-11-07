a8757e7e5e9337428ba06c669a3e1b38ec2aa561
Dan Shoemaker <dan@shoeman-tech.com>
2025-11-06 16:03:21 -0800

    completed module 157

---
+++ 16-Working_with_Images/157-Quick_note_on_example_images/main.py
@@ -0,0 +1,16 @@
+zz = "############################################################################"
+print(zz)
+
+aa = "Depending on when we downloaded the course material, the blue image may be corrupted"
+print(aa)
+bb = "To solve this problem either download the updated image from GitHub or run the following code:"
+print(bb)
+cc = "blue = Image.open('blue_color.png')"
+print(cc)
+dd = "blue_new = blue.convert('RGB')"
+print(dd)
+ee = "blue_new.save('blue_new.jpg')"
+print(ee)
+ff = "blue = Image.open('blue_new.jpg')"
+print(ff)
+print(zz)
