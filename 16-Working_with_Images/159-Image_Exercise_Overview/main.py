435e8109a62d76c40ebc093d33a5d076fcb0f5e0
Dan Shoemaker <dan@shoeman-tech.com>
2025-11-08 15:31:05 -0800

    completed module 159

---
+++ 16-Working_with_Images/159-Image_Exercise_Overview/main.py
@@ -0,0 +1,14 @@
+zz = "#######################################################"
+print(zz)
+
+aa = "In the folder 'Working with Images' are two images that we will be working with"
+print(aa)
+bb = "word_matrix.png"
+cc = "mask.png"
+print(bb)
+print(cc)
+dd = "The word_matrix is a .png image that contains a spreadsheet of words with a hidden message in it"
+print(dd)
+ee = "Your task is to use the mask.png image to reveal the hidden message inside the word_matrix.png"
+print(ee)
+print(zz)
