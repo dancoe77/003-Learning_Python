66c8552bbae09fc6a863425332612d8df1f2997e
Dan Shoemaker <dan@shoeman-tech.com>
2025-11-12 21:15:19 -0800

    completed module 161

---
+++ 17-Working_with_PDFs_and_Spreadsheets/161-Intro_to_PDFs_and_Spreadsheets/main.py
@@ -0,0 +1,6 @@
+zz = "##################################################################"
+print(zz)
+
+aa = "In this section we will explore libraries that will allow us to interact with these files"
+print(aa)
+print(zz)
