5de36a0f43466090e79ba45cc4943179ea2a43a8
Dan Shoemaker <dan@shoeman-tech.com>
2025-11-12 21:18:52 -0800

    completed module 162

---
+++ 17-Working_with_PDFs_and_Spreadsheets/162-Quick_note_on_UnicodeDecodeError/main.py
@@ -0,0 +1,6 @@
+zz = "########################################################################"
+print(zz)
+
+aa = "Depending on your location and your OS, you won't receive the UnicodeDecodeError."
+print(aa)
+print(zz)
