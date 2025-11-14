5f0fbfba4295aee5399bddbfed7fe33185234cbd
Dan Shoemaker <dan@shoeman-tech.com>
2025-11-13 23:04:55 -0800

    completed module 164

---
+++ 17-Working_with_PDFs_and_Spreadsheets/164-Import_note_on_PyPDF2/main.py
@@ -0,0 +1,18 @@
+zz = "###################################################################################"
+print(zz)
+
+aa = "Import note on PyPDF2"
+print(aa)
+bb = "Since this video was recorded, the development of PyPDF2 has officially shifted to the actively maintained pypdf library."
+print(bb)
+cc = "While the functionality is very similar, it's recommended to use pypdf for the latest updates, bug fixes, and features."
+print(cc)
+dd = "The concepts covered in this video still apply, only the name of the object changed."
+print(dd)
+ee = "Run pip install pypdf to install the latest version of pypdf."
+print(ee)
+ff = "Instead of importing PyPDF2 just import pypdf and you are ready to go."
+print(ff)
+gg = "If you don't want to update your code, you can also import pypdf as PyPDF2 which basically creates an alias from pypdf to PyPDF2."
+print(gg)
+print(zz)
