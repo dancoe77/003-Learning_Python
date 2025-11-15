248f4a41eeef944f8a837886e17ccfc6717961b4
Dan Shoemaker <dan@shoeman-tech.com>
2025-11-15 15:15:25 -0800

    completed module 168

---
+++ 18-Emails_with_Python/168-Intro_to_Emails/main.py
@@ -0,0 +1,8 @@
+zz = "#########################################################################"
+print(zz)
+
+aa = "We will explore how to send emails and how to check our inbox for received messages"
+print(aa)
+bb = "This process is highly dependent on having admin privileges on the local computer, internet and email"
+print(bb)
+print(zz)
