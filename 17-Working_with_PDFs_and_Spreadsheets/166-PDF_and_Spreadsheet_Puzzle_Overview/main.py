d5a942719272c86e35b7fd5091d24ef6fd0fa1eb
Dan Shoemaker <dan@shoeman-tech.com>
2025-11-14 18:13:10 -0800

    completed module 166

---
+++ 17-Working_with_PDFs_and_Spreadsheets/166-PDF_and_Spreadsheet_Puzzle_Overview/main.py
@@ -0,0 +1,8 @@
+zz = "###############################################################"
+print(zz)
+
+aa = "Task One: Use Python to extract the Google Drive link from the .csv file"
+print(aa)
+bb = "Task Two: Download the PDF from the Google Drive link and find the phone number that is in the document"
+print(bb)
+print(zz)
