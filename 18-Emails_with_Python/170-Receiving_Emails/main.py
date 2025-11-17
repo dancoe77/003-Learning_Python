5f2bc395d35e2e991bd1db19b16fe817b8b98be2
Dan Shoemaker <dan@shoeman-tech.com>
2025-11-16 17:28:04 -0800

    completed module 170

---
+++ 18-Emails_with_Python/170-Receiving_Emails/main.py
@@ -0,0 +1,24 @@
+import imaplib
+M = imaplib.IMAP4_SSL("imap.gmail.com")
+import getpass
+email = getpass.getpass('Enter your email address: ')
+password = getpass.getpass('Enter your password: ')
+M.login(email, password)
+# print(M.list())
+M.select("Inbox")
+# typ, data = M.search(None, "BEFORE 01-Nov-2000")
+# typ, data = M.search(None, "FROM user@example.com")
+typ, data = M.search(None, "SUBJECT 'NEW TEST PYTHON'")
+print(typ)
+print(data)
+email_id = data[0]
+result, email_data = M.fetch(email_id, "(RFC822)")
+print(email_data)
+raw_email = email_data[0][1]
+raw_email_string = raw_email.decode("utf-8")
+import email
+email_message = email.message_from_string(raw_email_string)
+for part in email_message.walk():
+    if part.get_content_type() == "text/plain":
+        body = part.get_payload(decode=True)
+        print(body)
