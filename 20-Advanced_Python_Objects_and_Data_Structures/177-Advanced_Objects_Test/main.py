fa7933868e9e396421bb40ac6f98cd737f6838af
Dan Shoemaker <dan@shoeman-tech.com>
2025-11-20 23:24:09 -0800

    completed module 177

---
+++ 20-Advanced_Python_Objects_and_Data_Structures/177-Advanced_Objects_Test/main.py
@@ -0,0 +1,34 @@
+zz = "#################################################################"
+print(zz)
+
+aa = "Problem 1: Convert 1024 to binary and hex"
+print(aa)
+bb = "Problem 2: Round 5.23222 to two decimal places"
+print(bb)
+cc = "Problem 3: Check if every letter in the string s is lower case"
+print(cc)
+s = "hello how are you Mary, are you feeling okay?"
+print(s)
+dd = "Problem 4: How many times does the letter 'w' show up in the string below?"
+print(dd)
+t = "twywywtwywbwhsjhwuwshshwuwwwjdjdid"
+print(t)
+ee = "Problem 5: Find the elements in set1 that are not in set2"
+print(ee)
+set1 = {2,3,1,5,6,8}
+set2 = {3,1,7,5,6,8}
+print(set1)
+print(set2)
+ff = "Problem 6: Find all the elements that are in either set"
+print(ff)
+gg = "Problem 7: Create this dictionary: {0:0, 1:1, 2:8, 3:27, 4:64}"
+print(gg)
+hh = "Problem 8: Reverse the list below:"
+print(hh)
+l = [1,2,3,4]
+print(l)
+ii = "Problem 9: Sort the list below:"
+print(ii)
+m = [3,4,2,5,1]
+print(m)
+print(zz)
