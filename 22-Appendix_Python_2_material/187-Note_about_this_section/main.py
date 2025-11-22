5ade85a32a2e960e67f9c602f5617feb7864fa61
Dan Shoemaker <dan@shoeman-tech.com>
2025-11-21 18:53:58 -0800

    completed module 187

---
+++ 22-Appendix_Python_2_material/187-Note_about_this_section/main.py
@@ -0,0 +1,14 @@
+zz = "##################################################################"
+print(zz)
+
+aa = "The following material is an appendix of the older Python 2 videos"
+print(aa)
+bb = "Many students requested that we keep these older videos here for reference"
+print(bb)
+cc = "If you've done the course linearly to this point, then you already know all the material in this section"
+print(cc)
+dd = "Feel free to mark these lectures as complete through the curriculum, you do not need to watch them"
+print(dd)
+ee = "More info: https://support.udemy.com/hc/en-us/articles/229607188-How-to-Mark-or-Unmark-Lectures-as-Complete"
+print(ee)
+print(zz)
