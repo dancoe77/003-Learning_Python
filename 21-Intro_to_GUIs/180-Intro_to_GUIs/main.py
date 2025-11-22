66f97ce35d4206294c9ae5ecfb21e6b4447fd911
Dan Shoemaker <dan@shoeman-tech.com>
2025-11-21 17:50:21 -0800

    completed module 180

---
+++ 21-Intro_to_GUIs/180-Intro_to_GUIs/main.py
@@ -0,0 +1,24 @@
+zz = "####################################################"
+print(zz)
+
+aa = "Welcome to the bonus GUI Section of this course!"
+print(aa)
+bb = "All notebooks associated with this section are located under the GUI folder in the NbViewer or Github repository containing the course notebooks"
+print(bb)
+cc = "There are many,many Graphical User Interface options for Python users."
+print(cc)
+dd = "Here is the link to the documentation: https://wiki.python.org/moin/GuiProgramming"
+print(dd)
+ee = "It's important to remember that no framework is objectively better than all the others, and different use cases will require different frameworks."
+print(ee)
+ff = "For instance, if you want to begin GUIs by designing a graphical game, your best bet would be PyGame!"
+print(ff)
+gg = "If you want to build web apps, you'll probably want to avoid Python as the GUI and use it for backend work using Flask or Django instead"
+print(gg)
+hh = "With web apps you'll want to explore HTML,CSS, and Javascript to provide the GUI for your user"
+print(hh)
+ii = "For the rest of this section we will explore the awesome world of Widgets in the Jupyter Notebook setting!"
+print(ii)
+jj = "This type of GUI is best suited for creating dashboards for data/business analysis situations"
+print(jj)
+print(zz)
