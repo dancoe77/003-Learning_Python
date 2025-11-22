06b3780d0c76de81e7bd2e8ab1f60e8a5806e668
Dan Shoemaker <dan@shoeman-tech.com>
2025-11-21 17:53:53 -0800

    completed module 181

---
+++ 21-Intro_to_GUIs/181-Quick_Note_about_ipywidgets/main.py
@@ -0,0 +1,8 @@
+zz = "#####################################################################"
+print(zz)
+
+aa = "Just a quick note about coloring and styling your ipywidgets, ipywidgets has actually switched over to a Bootstrap style, for more info, check out:"
+print(aa)
+bb = "https://ipywidgets.readthedocs.io/en/latest/examples/Widget%20Styling.html#Predefined-styles"
+print(bb)
+print(zz)
