3e0527f376e5a8e1153506c21532acda9fb182c1
Dan Shoemaker <dan@shoeman-tech.com>
2025-11-21 17:43:26 -0800

    completed module 179

---
+++ 21-Intro_to_GUIs/179-Quick_Note_if_Widgets_do_not_load/main.py
@@ -0,0 +1,10 @@
+zz = "#######################################################"
+print(zz)
+
+aa = "If the widget fails to load we can try two things:"
+print(aa)
+bb = "Upgrade the current installation using pip install -U ipywidgets"
+print(bb)
+cc = "Start jupyter notebook via the anaconda navigator and not via the command line"
+print(cc)
+print(zz)
